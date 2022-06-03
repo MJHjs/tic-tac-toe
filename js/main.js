ec59eaaecdba873b39443e34e78920fbb2c5817c
MJHFX <wunmastermind@gmail.com>
2022-06-03 10:36:25 -0500

    2nd commit , starting JS, i hate JS lol i dont hate it ; just hard to grasp so ill have alotta documentation and SOverflow help

--- js/main.js
+++ js/main.js
@@ -2,4 +2,14 @@
 /*----- app's state (variables) -----*/
 /*----- cached element references -----*/
 /*----- event listeners -----*/
+// becasue of poisition it will be processed before any HTML in the page
+/* The Array.from() method returns an array from any object with a length property.
+*/
+window.addEventListener('domContentLoaded', () => {
+    const tiles = Array.from(document.querySelectorAll('.tile'));
+    const playerDisplay = document.querySelector('.display-player');
+    const resetButton = document.querySelector('#reset');
+
+    resetButton.addEventListener('click, resetBoard');
+})
 /*----- functions -----*/
